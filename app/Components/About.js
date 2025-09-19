935f199ac82b9f264357cc48af3f08dde70c37fb
manjunathjk2406 <manjunathkavalikai9@gmail.com>
2025-09-19 21:38:20 +0530

    Update About.js

--- app/Components/About.js
+++ app/Components/About.js
@@ -10,7 +10,7 @@ const About = ({isDarkMode}) => {
     whileInView = {{opacity:1}} 
     transition={{duration:1}}  
     id="about" 
-    className='w-full px-[12%] py-10 scroll-mt-20'>
+    className='w-full px-[12%] py-10 scroll-mt-20 max-sm:h-[130rem]'>
         <motion.h4
         initial = {{opacity:0,y:-20}} 
         whileInView = {{opacity:1,y:0}} 
@@ -75,6 +75,7 @@ const About = ({isDarkMode}) => {
 }
 
 export default About
+
 
 
 
