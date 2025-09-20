372e4298791fe4969116217377e2a2b34a8989ca
manjunathjk2406 <manjunathkavalikai9@gmail.com>
2025-09-20 18:22:01 +0530

    Initial commit of existing project

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
               initial = {{y:30,opacity:0}} 
               whileInView = {{y:0,opacity:1}} 
               transition={{duration:0.6, delay:1}}  
-              href="./sample-resume.docx" download className='px-5 py-1 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume
+              href="./myresume.docx" download className='px-5 py-1 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume
                 <Image alt="" src={assets.download_icon} className='w-4'/>
               </motion.a>
             </div>
